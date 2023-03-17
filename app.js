a0c29734d8a2ede0d8ebef09d301f87f72b632e5
likeri29 <s.pochtaruk29@gmail.com>
2023-03-17 17:18:19 +0100

    Make an app, need to edit shadow on hover and request

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const button = document.querySelector('button');
+const adviceId = document.querySelector('p');
+const advice = document.querySelector('h1');
+
+const adviceGenerate = async () => {
+    try {
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+        adviceId.innerHTML = `Advice #${data.slip.id}`;
+        advice.innerHTML = data.slip.advice;
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+button.addEventListener('click', adviceGenerate);
+
